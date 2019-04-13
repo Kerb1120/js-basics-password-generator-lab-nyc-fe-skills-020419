644a9feec68e503d72802d117c04ecaa4967425a
Kerben Bermingham <b.kerben2@gmail.com>
2019-04-13 00:40:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 window.addEventListener("load", () => {
   //start your code here
+  
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const numbers = "0123456789"
+  const specialCharacters = "!#$%&()*+,-./:;<=>?@[\\]^_{|}~"
+  
+  function onSubmit(event) {
+    // prevent the default native behavior
+    event.preventDefault()
+    // get the thing that was entered
+    const input = document.getElementById('password-length')
+    console.log(input.value)
+    const howManyCharacters = input.value 
+    if (input.value < 4) {
+      howManyCharacters = 4
+    }
+    
+    let startingString = ''
+    for (let i = 0; i < howManyCharacters; i++) {
+    if(startingString.length < howManyCharacters) {
+    let randomIndex = Math.floor(Math.random() * lowerCaseLetters.length)
+    startingString += lowerCaseLetters[randomIndex]
+    }
+   if(startingString.length < howManyCharacters) {
+   randomIndex = Math.floor(Math.random() * upperCaseLetters.length)
+    startingString += upperCaseLetters[randomIndex]
+   }
+   if(startingString.length < howManyCharacters) {
+   randomIndex = Math.floor(Math.random() * numbers.length)
+    startingString += numbers[randomIndex]
+   }
+   if(startingString.length < howManyCharacters) {
+   randomIndex = Math.floor(Math.random() * specialCharacters.length)
+    startingString += specialCharacters[randomIndex]
+    }
+    }  
+    const output = document.getElementById('result')
+    output.innerHTML = startingString
+  }
+  
+  document.addEventListener('submit', onSubmit)
 });
